9780a230df8c8231a0f940acb666e26e45d3c22a
Ricardo <lxz13254@outlook.com>
2018-03-16 14:50:05 +0800

    3-16

--- CharityPlatform/src/main/java/com/seu/beyondtheboundary/charityplatform/controller/UserspaceController.java
+++ CharityPlatform/src/main/java/com/seu/beyondtheboundary/charityplatform/controller/UserspaceController.java
@@ -18,12 +18,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -36,6 +41,7 @@ import java.util.List;
 @RequestMapping("/u")
 public class UserspaceController {
 
+	public static final String root = "src/main/resources/static/userimages";
 
 	@Autowired
 	private ProjectService projectService;
@@ -102,6 +108,57 @@ public class UserspaceController {
 		userRepository.save(user1);
         return "redirect:/personal_center";
     }
+
+	@GetMapping("/user_commit_verify")
+	public String user_commit_verify() {
+		return "/person/complete_personal_information";
+	}
+
+	@PostMapping("/user_commit_verify")
+	public String user_commit_verify1(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, User user, HttpServletRequest request, HttpServletResponse response) {
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		//使用request对象的getSession()获取session，如果session不存在则创建一个
+		HttpSession session = request.getSession();
+		//将数据存储到session中
+		User user1 = (User) session.getAttribute("user");
+		user1.setSex(user.isSex());
+		if(user.getTel() != "")
+			user1.setTel(user.getTel());
+		if(user.getAddress() != "")
+			user1.setAddress(user.getAddress());
+		if(user.getUser_id_card() != "")
+			user1.setUser_id_card(user.getUser_id_card());
+		if(user.getRealname() != "")
+			user1.setRealname(user.getRealname());
+		if(user.getEmail() != "")
+			user1.setEmail(user.getEmail());
+
+		user1.setVerified(2);		//设为2，让该用户进入待审核列表
+
+		if(!file.isEmpty()) {
+			try {
+				Files.copy(file.getInputStream(), Paths.get(root, file.getOriginalFilename()));
+				redirectAttributes.addFlashAttribute("message", "you successfully uploaded " + file.getOriginalFilename() + "!");
+			} catch (IOException | RuntimeException e) {
+				redirectAttributes.addFlashAttribute("message", "Failed to upload " + file.getOriginalFilename() + " =>" + e.getMessage());
+			}
+		} else {
+			redirectAttributes.addFlashAttribute("message", "Failed to upload " + file.getOriginalFilename() + " because it was empty");
+		}
+
+
+		userRepository.save(user1);
+		return "redirect:/personal_center";
+	}
+
+
+	@GetMapping("/I_want_verify")
+	public String I_want_verify() {
+		return "/person/I_want_verify";
+	}
+
 }
 
 
